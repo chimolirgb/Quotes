4e275a0bf7cea82f683b413537b19bf94a9c245a
chimolirgb <chimolilucy@gmail.com>
2021-05-15 16:52:19 +0300

    modified files

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,5 @@
 export class Quote {
-    id: number;
+   id: number;
   name: string;
+  // description: string;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {id:1, name:'We become what we think about'},
+    {id:2,name:'If not us, who? If not now, when?'},
+  ];
 
   constructor() { }
 
